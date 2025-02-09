eec26828ae628fb2cd5a1ebae58584cc2001f4c6
aladin <micheljaheim5@proton.me>
2025-02-09 18:32:43 -0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,33 @@
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("DevConsoleVisible", true)
+print("Injecting...")
+wait(1)
+print([[
+                     
+                     
+DDDDDDDDDDDDD        
+D::::::::::::DDD     
+D:::::::::::::::DD   
+DDD:::::DDDDD:::::D  
+  D:::::D    D:::::D 
+  D:::::D     D:::::D
+  D:::::D     D:::::D
+  D:::::D     D:::::D
+  D:::::D     D:::::D
+  D:::::D     D:::::D
+  D:::::D     D:::::D
+  D:::::D    D:::::D 
+DDD:::::DDDDD:::::D  
+D:::::::::::::::DD   
+D::::::::::::DDD     
+DDDDDDDDDDDDD        
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+]])
+wait(10)
+StarterGui:SetCore("DevConsoleVisible", false)
